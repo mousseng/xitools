873e8f2e2051ae14616de31b33d9f6b37954f71e
mousseng <assaeva@gmail.com>
2018-12-24 23:19:25 -0500

    Draw skillchain on screen
    Skillchains are supposed to be properly tracked per target now - we
    detect breakages now. Additionally, we've got:
     - human-readable identifiers for enemies and weaponskills
     - a timer for the current skillchain stage
     - an `/sc clear` command for resetting the enemy tracker
     - and saveable settings
    
    Still need to track skillchain element; it appears to be an enum
    delivered with the action, but I'm not sure what's what yet.

--- addons/skillchain/skillchain.lua
+++ addons/skillchain/skillchain.lua
@@ -1,8 +1,9 @@
 _addon.author  = 'lin'
 _addon.name    = 'skillchain'
-_addon.version = '0.1.0'
+_addon.version = '0.2.0'
 
-local Packet = require 'packet'
+local Packet       = require 'packet'
+local Weaponskills = require 'weaponskills'
 
 require 'common'
 require 'timer'
@@ -10,20 +11,34 @@ require 'timer'
 local Party   = {}
 local Enemies = {}
 
--- local DefaultConfig = {
---     font = {
---         family    = 'Arial',
---         size      = 10,
---         color     = 0xFFFF0000,
---         position  = { 50, 125 },
---         bgcolor   = 0x80000000,
---         bgvisible = true
---     }
--- }
+local NonChainingSkills = {
+    [8] = true,
+    [36] = true,
+    [37] = true,
+    [79] = true,
+    [80] = true,
+    [87] = true,
+    [89] = true,
+    [143] = true,
+    [149] = true,
+    [150] = true,
+    [230] = true
+}
 
--- local SkillchainConfig = DefaultConfig
+local DefaultConfig = {
+    font = {
+        family    = 'Consolas',
+        size      = 10,
+        color     = 0xFFFF0000,
+        position  = { 50, 125 },
+        bgcolor   = 0x80000000,
+        bgvisible = true
+    }
+}
 
-function update_party(packet)
+local SkillchainConfig = DefaultConfig
+
+local function update_party(packet)
     local member1 = struct.unpack('i', packet, 0x08 + 1)
     local member2 = struct.unpack('i', packet, 0x08 + 1 + 12)
     local member3 = struct.unpack('i', packet, 0x08 + 1 + 24)
@@ -44,9 +59,38 @@ function update_party(packet)
     Party[0] = nil
 end
 
+local function get_entity(server_id)
+    -- entity array is 2304 items long
+    for x = 0, 2303 do
+        -- get the entity
+        local e = GetEntity(x)
+
+        -- ensure the entity is valid
+        if e ~= nil and e.WarpPointer ~= 0 then
+            if e.ServerId == server_id then
+                return e
+            end
+        end
+    end
+
+    return nil
+end
+
 function load()
+    SkillchainConfig = ashita.settings.load_merged(_addon.path .. 'settings/settings.json', SkillchainConfig)
+
     -- initialize rendering data
-    -- SkillchainConfig = ashita.settings.load_merged
+    local font = AshitaCore:GetFontManager():Create('__skillchain_addon')
+    font:SetColor(SkillchainConfig.font.color)
+    font:SetFontFamily(SkillchainConfig.font.family)
+    font:SetFontHeight(SkillchainConfig.font.size)
+    font:SetBold(false)
+    font:SetPositionX(SkillchainConfig.font.position[1])
+    font:SetPositionY(SkillchainConfig.font.position[2])
+    font:SetText('Skillchain ~ by lin')
+    font:SetVisibility(true)
+    font:GetBackground():SetColor(SkillchainConfig.font.bgcolor)
+    font:GetBackground():SetVisibility(SkillchainConfig.font.bgvisible)
 
     -- initialize party data
     local party = AshitaCore:GetDataManager():GetParty()
@@ -71,8 +115,35 @@ function load()
     Party[0] = nil
 end
 
+function unload()
+    local font = AshitaCore:GetFontManager():Get('__skillchain_addon')
+    SkillchainConfig.font.position = { font:GetPositionX(), font:GetPositionY() }
+
+    -- save config
+    ashita.settings.save(_addon.path .. 'settings/settings.json', SkillchainConfig)
+
+    -- unload font object
+    AshitaCore:GetFontManager():Delete('__skillchain_addon')
+end
+
 function render()
-    -- TODO
+    local font = AshitaCore:GetFontManager():Get('__skillchain_addon')
+    local resx = AshitaCore:GetResourceManager()
+    local e = T{}
+
+    for k, v in pairs(Enemies) do
+        local line = v.name
+
+        for x = 1, #v.chain do
+            line = line .. '\n   > ' .. Weaponskills[v.chain[x].id]
+        end
+
+        line = line .. '  ' .. (6 - math.abs(v.time - os.time())) .. 's'
+
+        table.insert(e, line)
+    end
+
+    font:SetText(e:concat('\n'))
 end
 
 function handle_command(cmd)
@@ -109,19 +180,24 @@ function handle_packet(id, size, packet)
             for j = 1, target.action_count do
                 local weaponskill = target.actions[j]
 
-                if Party[action.actor_id] == true then
-                    -- are we starting a new chain?
-                    if Enemies[target.id] == nil then
-                        Enemies[target.id] = {}
-                        table.insert(Enemies[target.id], weaponskill.id)
-                    -- reset timer if not
-                    else
-                        ashita.timer.remove_timer(target.id)
+                -- bail out if it's a non-chaining weaponskill, or if not from our party
+                if Party[action.actor_id] and not NonChainingSkills[weaponskill.animation] then
+                    -- are we breaking a chain, or starting a new one?
+                    if Enemies[target.id] == nil or not weaponskill.has_add_effect then
+                        Enemies[target.id] = {
+                            name = get_entity(target.id).Name,
+                            time = nil,
+                            chain = {}
+                        }
                     end
 
                     -- once the chain window closes after ~6s,
                     -- drop the chain information
-                    ashita.timer.create(target.id, 6, 1, function(tgt) Enemies[tgt] = nil end, target.id)
+                    ashita.timer.remove_timer(target.id)
+                    ashita.timer.create(target.id, 7, 1, function(tgt) Enemies[target.id] = nil end, target.id)
+
+                    Enemies[target.id].time = os.time()
+                    table.insert(Enemies[target.id].chain, { id = weaponskill.animation, element = nil })
                 end
             end
         end
@@ -131,6 +207,7 @@ function handle_packet(id, size, packet)
 end
 
 ashita.register_event('load', load)
+ashita.register_event('unload', unload)
 ashita.register_event('render', render)
 ashita.register_event('command', handle_command)
 ashita.register_event('incoming_packet', handle_packet)
