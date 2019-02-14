ed7dec65b97cdbeff80d35ed35c3808d5d6c2911
mousseng <assaeva@gmail.com>
2019-02-13 22:40:51 -0500

    Add ashitacast configs
    Since I've started using AshitaCast, may as well keep those configs
    under source control as well. Updated the install script to also copy
    those files over.

--- install.ps1
+++ install.ps1
@@ -1,4 +1,7 @@
-$src = "$PSScriptRoot\addons\"
+$addons = "$PSScriptRoot\addons\"
+$config = "$PSScriptRoot\config\"
+
 $tgt = "$($env:LocalAppData)\PlayOnline\Ashita\"
 
-Copy-Item -Recurse -Container -Force -Path $src -Destination $tgt
+Copy-Item -Recurse -Container -Force -Path $addons -Destination $tgt
+Copy-Item -Recurse -Container -Force -Path $config -Destination $tgt
