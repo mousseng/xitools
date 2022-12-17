ce2c7d68dac51cd275b59d7eec5fa36a58307178
mousseng <lin@sivvi.net>
2022-12-16 19:52:36 -0500

    update install path

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 $addons = "$PSScriptRoot\addons\"
 $config = "$PSScriptRoot\config\"
 
-$tgt = "$($env:LocalAppData)\PlayOnline\Ashita\"
+$tgt = "$($env:LocalAppData)\HorizonXI\Game\"
 
 Copy-Item -Recurse -Container -Force -Path $addons -Destination $tgt
 Copy-Item -Recurse -Container -Force -Path $config -Destination $tgt
