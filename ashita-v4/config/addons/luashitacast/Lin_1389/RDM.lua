48a96d48f019f76224f2de0b226976ddc936a163
mousseng <lin@sivvi.net>
2022-12-18 13:01:44 -0500

    pull in fresh RDM lua config

---
+++ ashita-v4/config/addons/luashitacast/Lin_1389/RDM.lua
@@ -0,0 +1,42 @@
+local profile = {};
+profile.Sets = {
+};
+
+profile.Packer = {
+};
+
+profile.OnLoad = function()
+    gSettings.AllowAddSet = true;
+end
+
+profile.OnUnload = function()
+end
+
+profile.HandleCommand = function(args)
+end
+
+profile.HandleDefault = function()
+end
+
+profile.HandleAbility = function()
+end
+
+profile.HandleItem = function()
+end
+
+profile.HandlePrecast = function()
+end
+
+profile.HandleMidcast = function()
+end
+
+profile.HandlePreshot = function()
+end
+
+profile.HandleMidshot = function()
+end
+
+profile.HandleWeaponskill = function()
+end
+
+return profile;
