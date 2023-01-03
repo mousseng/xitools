f6612c4fa0f4b4e0e6dfc86b1a14c74e51968ddb
mousseng <lin@sivvi.net>
2023-01-03 18:40:27 -0500

    config: update RDM

--- config/addons/luashitacast/Lin_1389/RDM.lua
+++ config/addons/luashitacast/Lin_1389/RDM.lua
@@ -1,21 +1,84 @@
-local profile = {};
-profile.Sets = {
-};
+require 'common'
+local chat = require('chat')
 
-profile.Packer = {
-};
+local profile = {
+    Sets = {
+        Base = {
+            Main = "Centurion's sword",
+            -- Sub = "",
+            -- Range = "",
+            -- Ammo = "",
+            -- Head = "",
+            Body = "Royal Footman's tunic",
+            Hands = "Royal Footman's gloves",
+            Legs = "Royal Footman's trousers",
+            Feet = "Royal Footman's clogs",
+            Neck = "Tiger stole",
+            Waist = "Friar's rope",
+            -- Ear1 = "",
+            -- Ear2 = "",
+            Ring1 = "San d'Orian ring",
+            -- Ring2 = "",
+            Back = "Cotton cape",
+        },
+        Resting = {
+            Main = "Pilgrim's wand",
+        },
+        Tp = { },
+        Movement = { },
+    },
+    Packer = {
+    },
+}
 
 profile.OnLoad = function()
-    gSettings.AllowAddSet = true;
+    local player = gData.GetPlayer()
+    gSettings.AllowAddSet = true
+    gSettings.SoloMode = false
+    AshitaCore:GetChatManager():QueueCommand(1, '/macro book 2')
+
+    if player.SubJob == 'WHM' then
+        AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1')
+    elseif player.SubJob == 'BLM' then
+        AshitaCore:GetChatManager():QueueCommand(1, '/macro set 2')
+    elseif player.SubJob == 'THF' then
+        AshitaCore:GetChatManager():QueueCommand(1, '/macro set 3')
+    elseif player.SubJob == 'NIN' then
+        AshitaCore:GetChatManager():QueueCommand(1, '/macro set 4')
+    end
 end
 
 profile.OnUnload = function()
 end
 
 profile.HandleCommand = function(args)
+    if #args == 0 then return end
+    if args[1] == 'solo' and not gSettings.SoloMode then
+        gSettings.SoloMode = true
+        print(chat.header('LAC: RDM'):append(chat.message('enabling solo mode')))
+        AshitaCore:GetChatManager():QueueCommand(-1, '/lac disable Main')
+        AshitaCore:GetChatManager():QueueCommand(-1, '/lac disable Sub')
+        AshitaCore:GetChatManager():QueueCommand(-1, '/lac disable Range')
+    elseif args[1] == 'solo' and gSettings.SoloMode then
+        gSettings.SoloMode = false
+        print(chat.header('LAC: RDM'):append(chat.message('disabling solo mode')))
+        AshitaCore:GetChatManager():QueueCommand(-1, '/lac enable Main')
+        AshitaCore:GetChatManager():QueueCommand(-1, '/lac enable Sub')
+        AshitaCore:GetChatManager():QueueCommand(-1, '/lac enable Range')
+    end
 end
 
 profile.HandleDefault = function()
+    local player = gData.GetPlayer()
+    if player.IsMoving then
+        gFunc.EquipSet('Movement')
+    elseif player.Status == 'Resting' then
+        gFunc.EquipSet('Resting')
+    elseif player.Status == 'Engaged' then
+        gFunc.EquipSet('Tp')
+    elseif player.Status == 'Idle' then
+        gFunc.EquipSet('Base')
+    end
 end
 
 profile.HandleAbility = function()
@@ -25,9 +88,13 @@ profile.HandleItem = function()
 end
 
 profile.HandlePrecast = function()
+    -- TODO: get fast-cast gear
 end
 
 profile.HandleMidcast = function()
+    -- TODO: get wand for early-game casting
+    -- TODO: get staves for late-game casting
+    gFunc.Equip('Neck', 'Justice Badge')
 end
 
 profile.HandlePreshot = function()
@@ -37,6 +104,7 @@ profile.HandleMidshot = function()
 end
 
 profile.HandleWeaponskill = function()
+    -- TODO: get str/dex/int/mnd gear
 end
 
-return profile;
+return profile
