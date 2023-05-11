7ba7c4d022ad51000094bc8aabc60b2a755809fa
mousseng <lin@sivvi.net>
2023-05-11 15:03:20 -0400

    config stuff

--- config/addons/luashitacast/Lin_1389/RDM.lua
+++ config/addons/luashitacast/Lin_1389/RDM.lua
@@ -207,7 +207,7 @@ profile.HandleDefault = function()
         gFunc.Equip('Body', "Kingdom Aketon")
     end
 
-    if player.Status == 'Resting' and player.MPP > 99 then
+    if player.Status == 'Resting' and player.MPP < 99 then
         gFunc.EquipSet('Rest')
     elseif player.Status == 'Engaged' then
         gFunc.EquipSet('Tp')
