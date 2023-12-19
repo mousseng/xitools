5d329d66cd4907dab3a1c48e4926185f69df7778
mousseng <lin@sivvi.net>
2023-12-19 13:42:56 -0500

    remove addons from install script (just use symlinks)

--- install.ps1
+++ install.ps1
@@ -1,9 +1,7 @@
-$addons = "$PSScriptRoot\addons\"
 $config = "$PSScriptRoot\config\"
 $scripts = "$PSScriptRoot\scripts\"
 
 $tgt = "$($env:LocalAppData)\HorizonXI\Game\"
 
-Copy-Item -Recurse -Container -Force -Path $addons -Destination $tgt
 Copy-Item -Recurse -Container -Force -Path $config -Destination $tgt -Exclude *.png
 Copy-Item -Recurse -Container -Force -Path $scripts -Destination $tgt
